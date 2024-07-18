f7797d3d52f501d8734a163b890fc244ed7dac14
uoyg <35022423+AreaSaku@users.noreply.github.com>
2024-07-18 13:47:26 +0900

    2024.7.0-beta.1-WKH0.86b
    Co-Authored-By: hisa <128118344+yurihisada@users.noreply.github.com>

--- packages/frontend/src/components/MkSignin.vue
+++ packages/frontend/src/components/MkSignin.vue
@@ -87,7 +87,7 @@ const host = ref(toUnicode(configHost));
 const totpLogin = ref(false);
 const isBackupCode = ref(false);
 const queryingKey = ref(false);
-const credentialRequest = ref<CredentialRequestOptions | null>(null);
+let credentialRequest: CredentialRequestOptions | null = null;
 
 const emit = defineEmits<{
 	(ev: 'login', v: any): void;
@@ -122,14 +122,14 @@ function onLogin(res: any): Promise<void> | void {
 }
 
 async function queryKey(): Promise<void> {
-	if (credentialRequest.value == null) return;
+	if (credentialRequest == null) return;
 	queryingKey.value = true;
-	await webAuthnRequest(credentialRequest.value)
+	await webAuthnRequest(credentialRequest)
 		.catch(() => {
 			queryingKey.value = false;
 			return Promise.reject(null);
 		}).then(credential => {
-			credentialRequest.value = null;
+			credentialRequest = null;
 			queryingKey.value = false;
 			signing.value = true;
 			return misskeyApi('signin', {
@@ -160,7 +160,7 @@ function onSubmit(): void {
 			}).then(res => {
 				totpLogin.value = true;
 				signing.value = false;
-				credentialRequest.value = parseRequestOptionsFromJSON({
+				credentialRequest = parseRequestOptionsFromJSON({
 					publicKey: res,
 				});
 			})
@@ -236,12 +236,14 @@ function openRemote(options: OpenOnRemoteOptions, targetHost?: string): void {
 	switch (options.type) {
 		case 'web':
 		case 'lookup': {
-			let _path = options.path;
+			let _path: string;
 
 			if (options.type === 'lookup') {
-				// TODO: v2024.2.0以降が浸透してきたら正式なURLに変更する▼
+				// TODO: v2024.7.0以降が浸透してきたら正式なURLに変更する▼
 				// _path = `/lookup?uri=${encodeURIComponent(_path)}`;
-				_path = `/authorize-follow?acct=${encodeURIComponent(_path)}`;
+				_path = `/authorize-follow?acct=${encodeURIComponent(options.url)}`;
+			} else {
+				_path = options.path;
 			}
 
 			if (targetHost) {
@@ -252,7 +254,7 @@ function openRemote(options: OpenOnRemoteOptions, targetHost?: string): void {
 			break;
 		}
 		case 'share': {
-			const params = query(options.params);			
+			const params = query(options.params);
 			if (targetHost) {
 				window.open(`https://${targetHost}/share?${params}`, '_blank', 'noopener');
 			} else {
