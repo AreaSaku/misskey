dd53c04c6dbda3a66431bf1712962edabbcd10de
uoyg <35022423+AreaSaku@users.noreply.github.com>
2024-09-07 18:10:41 +0900

    Update privacy.vue

--- packages/frontend/src/pages/settings/privacy.vue
+++ packages/frontend/src/pages/settings/privacy.vue
@@ -48,9 +48,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 				<div class="_gaps_m">
 					<MkSelect v-model="defaultNoteVisibility">
 						<option value="public">{{ i18n.ts._visibility.public }}</option>
-						<option value="home">{{ i18n.ts._visibility.home }}</option>
-						<option value="followers">{{ i18n.ts._visibility.followers }}</option>
-						<option value="specified">{{ i18n.ts._visibility.specified }}</option>
 					</MkSelect>
 					<MkSwitch v-model="defaultNoteLocalOnly">{{ i18n.ts._visibility.disableFederation }}</MkSwitch>
 				</div>
