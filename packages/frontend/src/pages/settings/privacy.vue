bc8ba5df32a66cd6a7aea4600bb6870654dc6e1d
uoyg <35022423+AreaSaku@users.noreply.github.com>
2025-12-12 18:16:22 +0900

    2025.12.1-alpha.0-WKH

--- packages/frontend/src/pages/settings/privacy.vue
+++ packages/frontend/src/pages/settings/privacy.vue
@@ -288,8 +288,8 @@ function save() {
 		makeNotesHiddenBefore: makeNotesHiddenBefore.value,
 		hideOnlineStatus: !!hideOnlineStatus.value,
 		publicReactions: !!publicReactions.value,
-		followingVisibility: followingVisibility.public,
-		followersVisibility: followersVisibility.public,
+		followingVisibility: followingVisibility.value,
+		followersVisibility: followersVisibility.value,
 		chatScope: chatScope.value,
 	});
 }
