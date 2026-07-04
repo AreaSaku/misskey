62dc100cfbaee9002896896a704bc0b79b5dc3dd
uoyg <35022423+AreaSaku@users.noreply.github.com>
2026-07-04 13:53:05 +0900

    2026.7.0-alpha.0-WKH

--- packages/frontend/src/preferences/def.ts
+++ packages/frontend/src/preferences/def.ts
@@ -479,19 +479,19 @@ export const PREF_DEF = definePreferences({
 		default: false,
 	},
 	'sound.on.note': {
-		default: { type: 'syuilo/n-aec', volume: 1 } as SoundStore,
+		default: { type: 'cx/無音', volume: 1 } as SoundStore,
 	},
 	'sound.on.noteMy': {
-		default: { type: 'syuilo/n-cea-4va', volume: 1 } as SoundStore,
+		default: { type: 'cx/New-My-Posts-ririse', volume: 1 } as SoundStore,
 	},
 	'sound.on.notification': {
-		default: { type: 'syuilo/n-ea', volume: 1 } as SoundStore,
+		default: { type: 'cx/無音', volume: 1 } as SoundStore,
 	},
 	'sound.on.reaction': {
-		default: { type: 'syuilo/bubble2', volume: 1 } as SoundStore,
+		default: { type: 'cx/無音', volume: 1 } as SoundStore,
 	},
 	'sound.on.chatMessage': {
-		default: { type: 'syuilo/waon', volume: 1 } as SoundStore,
+		default: { type: 'cx/無音', volume: 1 } as SoundStore,
 	},
 
 	'deck.alwaysShowMainColumn': {
