2a3105c1d653b396a8bfbc415f923e0f8f0aa837
uoyg <35022423+AreaSaku@users.noreply.github.com>
2026-09-25 23:02:07 +0900

    2026.9.0

--- packages/backend/src/core/UserFollowingService.ts
+++ packages/backend/src/core/UserFollowingService.ts
@@ -380,7 +380,7 @@ export class UserFollowingService implements OnModuleInit {
 
 		this.cacheService.userFollowingsCache.refresh(follower.id);
 
-		await this.decrementFollowing(following.follower, following.followee);
+		this.decrementFollowing(following.follower, following.followee);
 
 		if (!silent && this.userEntityService.isLocalUser(follower)) {
 			// Publish unfollow event
